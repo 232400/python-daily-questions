2bd7b3a0ec0fb892b09482545532bcb1c0224e6d
232400 <112846424+232400@users.noreply.github.com>
2022-10-08 09:39:42 +0530

    Add files via upload

---
+++ student users.py
@@ -0,0 +1,19 @@
+a=int(input("total user:"))
+if(a<0):
+    print("INVALID INPUT")
+elif(a==0):
+    print("there is no staff and studentuser")
+else:
+    b=int(input("staff user:"))
+    if(b<0):
+        print("INVALID INPUT")
+    elif(b==0):
+        print("student user=",a)
+    elif(a<b):
+        print("INVALID INPUT")
+    elif(a==b):
+        print("student user=0")
+    else:
+        c=a-b
+        d=c-b/3
+        print("student user=",int(d))
